eb6581da5884712c5cb21698b371e60d1fd68106
garfield.kim(김규범)/kakao <dolgodolah@gmail.com>
2021-10-05 23:35:31 +0900

    feat: 잘못된 값 입력할 경우 에러 메세지 출력

--- src/main/java/baseball/domain/BaseballManager.java
+++ src/main/java/baseball/domain/BaseballManager.java
@@ -7,6 +7,7 @@ public class BaseballManager {
 
     private static final String RESTART = "1";
     private static final String EXIT = "2";
+    private static final String INPUT_NUMBER_FORMAT = "^[1-9]{3}$";
 
     private Player player;
     private Computer computer;
@@ -27,9 +28,15 @@ public class BaseballManager {
 
     private void playBaseball() {
         String input = Input.inputNumber();
-        Result result = computer.calculateResult(input);
-        Output.printResult(result);
-        checkVictory(result);
+
+        try {
+            validateInputNumber(input);
+            Result result = computer.calculateResult(input);
+            Output.printResult(result);
+            checkVictory(result);
+        } catch (IllegalArgumentException e) {
+            Output.printErrorMessage();
+        }
     }
 
     private void checkVictory(Result result) {
@@ -48,12 +55,29 @@ public class BaseballManager {
     }
 
     private void checkRestartStatus(String input) {
+        try {
+            validateInputRestartStatus(input);
+        } catch (IllegalArgumentException e) {
+            Output.printErrorMessage();
+        }
+
         if (input.equals(RESTART)) {
             player.ready();
         }
-
         if (input.equals(EXIT)) {
             player.exit();
+        }
+    }
+
+    private void validateInputNumber(String input) {
+        if (!input.matches(INPUT_NUMBER_FORMAT)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInputRestartStatus(String input) {
+        if (!input.equals(RESTART) && !input.equals(EXIT)) {
+            throw new IllegalArgumentException();
         }
     }
 }
--- src/main/java/baseball/ui/Output.java
+++ src/main/java/baseball/ui/Output.java
@@ -7,6 +7,7 @@ public class Output {
     private static String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String VICTORY_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
+    private static final String ERROR_MESSAGE = "[ERROR] 올바른 값을 입력해주세요.";
 
     public static final String NOTHING = "낫싱";
     public static final String STRIKE = "스트라이크 ";
@@ -26,5 +27,9 @@ public class Output {
 
     public static void printResult(Result result) {
         System.out.println(result.toString());
+    }
+
+    public static void printErrorMessage() {
+        System.out.println(ERROR_MESSAGE);
     }
 }
