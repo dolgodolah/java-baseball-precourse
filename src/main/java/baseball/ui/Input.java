eba100d80d975ca63d7981e8fcdf9b67f736391c
garfield.kim(김규범)/kakao <dolgodolah@gmail.com>
2021-10-05 22:53:20 +0900

    feat: 상황별 입력값 요구하는 메세지 출력

--- src/main/java/baseball/ui/Input.java
+++ src/main/java/baseball/ui/Input.java
@@ -5,6 +5,12 @@ import nextstep.utils.Console;
 public class Input {
 
     public static String inputNumber() {
+        Output.printInputNumberMessage();
+        return Console.readLine();
+    }
+
+    public static String inputRestartStatus() {
+        Output.printRestartStatusMessage();
         return Console.readLine();
     }
 }
