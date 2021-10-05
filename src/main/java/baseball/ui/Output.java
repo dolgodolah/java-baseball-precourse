a270f0e998367c873189bf21694d39d75bdcb2e3
garfield.kim(김규범)/kakao <dolgodolah@gmail.com>
2021-10-05 23:36:51 +0900

    refactor: ui 개선

--- src/main/java/baseball/ui/Output.java
+++ src/main/java/baseball/ui/Output.java
@@ -4,7 +4,7 @@ import baseball.domain.Result;
 
 public class Output {
 
-    private static String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String VICTORY_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
     private static final String ERROR_MESSAGE = "[ERROR] 올바른 값을 입력해주세요.";
@@ -14,7 +14,7 @@ public class Output {
     public static final String BALL = "볼";
 
     public static void printInputNumberMessage() {
-        System.out.println(INPUT_NUMBER_MESSAGE);
+        System.out.print(INPUT_NUMBER_MESSAGE);
     }
 
     public static void printVictoryMessage() {
